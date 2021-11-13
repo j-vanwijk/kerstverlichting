8853e14c7b73011cb47f133f320569dfd9e16996
j-vanwijk <60421659+j-vanwijk@users.noreply.github.com>
2021-11-13 14:12:01 +0100

    deel 2

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 basic.forever(function () {
     pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P0, 0)
     basic.pause(200)
     pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P0, 1)
     basic.pause(200)
 })
